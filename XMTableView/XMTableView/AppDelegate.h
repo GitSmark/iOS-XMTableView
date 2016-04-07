d239f447d4d85edd311b597cee23fea5510e74bb
huangxy <myapple@MyappletekiMac.local>
2016-04-07 23:16:33 +0800

    Initialized XMTableView

---
+++ XMTableView/XMTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMTableView
+//
+//  Created by huangxy on 16/4/7.
+//  Copyright © 2016年 huangxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
